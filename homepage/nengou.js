ca49dfe4d61357ef606d57cf424c5479304994d9
F22BA019 <116255672+F22BA019@users.noreply.github.com>
2023-02-09 16:46:42 +0900

    Add files via upload

---
+++ homepage/nengou.js
@@ -0,0 +1,409 @@
+function botan() {
+    let txt = document.getElementById("in").value;
+    let result = "";
+
+    switch(txt) {
+        case "1897" :
+            result = "明治30年";
+        break;
+        case "1898" :
+            result = "明治31年";
+        break;
+        case "1899" :
+            result = "明治32年";
+        break;
+        case "1900" :
+            result = "明治33年";
+        break;
+        case "1901" :
+            result = "明治34年";
+        break;
+        case "1902" :
+            result = "明治35年";
+        break;
+        case "1903" :
+            result = "明治36年";
+        break;
+        case "1904" :
+            result = "明治37年";
+        break;
+        case "1905" :
+            result = "明治38年";
+        break;
+        case "1906" :
+            result = "明治39年";
+        break;
+        case "1907" :
+            result = "明治40年";
+        break;
+        case "1908" :
+            result = "明治41年";
+        break;
+        case "1909" :
+            result = "明治42年";
+        break;
+        case "1910" :
+            result = "明治43年";
+        break;
+        case "1911" :
+            result = "明治44年";
+        break;
+        case "1912" :
+            result = "明治45年,大正1年";
+        break;
+        case "1913" :
+            result = "大正2年";
+        break;
+        case "1914" :
+            result = "大正3年";
+        break;
+        case "1915" :
+            result = "大正4年";
+        break;
+        case "1916" :
+            result = "大正5年";
+        break;
+        case "1917" :
+            result = "大正6年";
+        break;
+        case "1918" :
+            result = "大正7年";
+        break;
+        case "1919" :
+            result = "大正8年";
+        break;
+        case "1920" :
+            result = "大正9年";
+        break;
+        case "1921" :
+            result = "大正10年";
+        break;
+        case "1922" :
+            result = "大正11年";
+        break;
+        case "1923" :
+            result = "大正12年";
+        break;
+        case "1924" :
+            result = "大正13年";
+        break;
+        case "1925" :
+            result = "大正14年";
+        break;
+        case "1926" :
+            result = "大正15年,昭和1年";
+        break;
+        case "1927" :
+            result = "昭和2年";
+        break;
+        case "1928" :
+            result = "昭和3年";
+        break;
+        case "1929" :
+            result = "昭和4年";
+        break;
+        case "1930" :
+            result = "昭和5年";
+        break;
+        case "1931" :
+            result = "昭和6年";
+        break;
+        case "1932" :
+            result = "昭和7年";
+        break;
+        case "1933" :
+            result = "昭和8年";
+        break;
+        case "1934" :
+            result = "昭和9年";
+        break;
+        case "1935" :
+            result = "昭和10年";
+        break;
+        case "1936" :
+            result = "昭和11年";
+        break;
+        case "1937" :
+            result = "昭和12年";
+        break;
+        case "1938" :
+            result = "昭和13年";
+        break;
+        case "1939" :
+            result = "昭和14年";
+        break;
+        case "1940" :
+            result = "昭和15年";
+        break;
+        case "1941" :
+            result = "昭和16年";
+        break;
+        case "1942" :
+            result = "昭和17年";
+        break;
+        case "1943" :
+            result = "昭和18年";
+        break;
+        case "1944" :
+            result = "昭和19年";
+        break;
+        case "1945" :
+            result = "昭和20年";
+        break;
+        case "1946" :
+            result = "昭和21年";
+        break;
+        case "1947" :
+            result = "昭和22年";
+        break;
+        case "1948" :
+            result = "昭和23年";
+        break;
+        case "1949" :
+            result = "昭和24年";
+        break;
+        case "1950" :
+            result = "昭和25年";
+        break;
+        case "1951" :
+            result = "昭和26年";
+        break;
+        case "1952" :
+            result = "昭和27年";
+        break;
+        case "1953" :
+            result = "昭和28年";
+        break;
+        case "1954" :
+            result = "昭和29年";
+        break;
+        case "1955" :
+            result = "昭和30年";
+        break;
+        case "1956" :
+            result = "昭和31年";
+        break;
+        case "1957" :
+            result = "昭和32年";
+        break;
+        case "1958" :
+            result = "昭和33年";
+        break;
+        case "1959" :
+            result = "昭和34年";
+        break;
+        case "1960" :
+            result = "昭和35年";
+        break;
+        case "1961" :
+            result = "昭和36年";
+        break;
+        case "1962" :
+            result = "昭和37年";
+        break;
+        case "1963" :
+            result = "昭和38年";
+        break;
+        case "1964" :
+            result = "昭和39年";
+        break;
+        case "1965" :
+            result = "昭和40年";
+        break;
+        case "1966" :
+            result = "昭和41年";
+        break;
+        case "1967" :
+            result = "昭和42年";
+        break;
+        case "1968" :
+            result = "昭和43年";
+        break;
+        case "1969" :
+            result = "昭和44年";
+        break;
+        case "1970" :
+            result = "昭和45年";
+        break;
+        case "1971" :
+            result = "昭和46年";
+        break;
+        case "1972" :
+            result = "昭和47年";
+        break;
+        case "1973" :
+            result = "昭和48年";
+        break;
+        case "1974" :
+            result = "昭和49年";
+        break;
+        case "1975" :
+            result = "昭和50年";
+        break;
+        case "1976" :
+            result = "昭和51年";
+        break;
+        case "1977" :
+            result = "昭和52年";
+        break;
+        case "1978" :
+            result = "昭和53年";
+        break;
+        case "1979" :
+            result = "昭和54年";
+        break;
+        case "1980" :
+            result = "昭和55年";
+        break;
+        case "1981" :
+            result = "昭和56年";
+        break;
+        case "1982" :
+            result = "昭和57年";
+        break;
+        case "1983" :
+            result = "昭和58年";
+        break;
+        case "1984" :
+            result = "昭和59年";
+        break;
+        case "1985" :
+            result = "昭和60年";
+        break;
+        case "1986" :
+            result = "昭和61年";
+        break;
+        case "1987" :
+            result = "昭和62年";
+        break;
+        case "1988" :
+            result = "昭和63年";
+        break;
+        case "1989" :
+            result = "昭和64年,平成1年";
+        break;
+        case "1990" :
+            result = "平成2年";
+        break;
+        case "1991" :
+            result = "平成3年";
+        break;
+        case "1992" :
+            result = "平成4年";
+        break;
+        case "1993" :
+            resut = "平成5年";
+        break;
+        case "1994" :
+            result = "平成6年";
+        break;
+        case "1995" :
+            result = "平成7年";
+        break;
+        case "1996" :
+            result = "平成8年";
+        break;
+        case "1997" :
+            result = "平成9年";
+        break;
+        case "1998" :
+            result = "平成10年";
+        break;
+        case "1999" :
+            result = "平成11年";
+        break;
+        case "2000" :
+            result = "平成12年";
+        break;
+        case "2001" :
+            result = "平成13年";
+        break;
+        case "2002" :
+            result = "平成14年";
+        break;
+        case "2003" :
+            result = "平成15年";
+        break;
+        case "2004" :
+            result = "平成16年";
+        break;
+        case "2005" :
+            result = "平成17年";
+        break;
+        case "2006" :
+            result = "平成18年";
+        break;
+        case "2007" :
+            result = "平成19年";
+        break;
+        case "2008" :
+            result = "平成20年";
+        break;
+        case "2009" :
+            result = "平成21年";
+        break;
+        case "2010" :
+            result = "平成22年";
+        break;
+        case "2011" :
+            result = "平成23年";
+        break;
+        case "2012" :
+            result = "平成24年";
+        break;
+        case "2013" :
+            result = "平成25年";
+        break;
+        case "2014" :
+            result = "平成26年";
+        break;
+        case "2015" :
+            result = "平成27年";
+        break;
+        case "2016" :
+            result = "平成28年";
+        break;
+        case "2017" :
+            result = "平成29年";
+        break;
+        case "2018" :
+            result = "平成30年";
+        break;
+        case "2019" :
+            result = "平成31年,令和1年";
+        break;
+        case "2020" :
+            result = "令和2年";
+        break;
+        case "2021" :
+            result = "令和3年";
+        break;
+        case "2022" :
+            result = "令和4年";
+        break;
+        case "2023" :
+            result = "令和5年";
+        break;
+        case "2024" :
+            result = "令和6年";
+        break;
+        case "2025" :
+            result = "令和7年";
+        break;
+        case "2026" :
+            result = "令和8年";
+        break;
+        case "2027" :
+            result = "令和9年";
+        break;
+        case "2028" :
+            result = "令和10年";
+        break;
+        default:
+            result = "×";
+            alert("それは表示できません");
+        break;
+    }
+
+    document.getElementById("out").value = result;
+}
